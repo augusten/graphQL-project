624c1596ccec8b4c43f95a7b152b042cb9f1c9b3
Auguste Nausedaite <augustenausedaite@Administrators-MacBook-Pro-2.local>
2018-08-27 15:28:40 +0200

    add integrations to the data model and CRD queries

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -10,18 +10,42 @@ const addProject = function (parent, args, context, info) {
 const updateProject = function (parent, args, context, info) {
   if (!args.name) throw new Error(`There is no name in the input, so there's no such project`);
 
-  return context.db.mutation.updateProject({ data: { name: args. name }, where: { id: args.id } }, info); //{where:{ id: args.id }}, {data: { name: args.name }}, info)
+  return context.db.mutation.updateProject({ data: { name: args. name }, where: { id: args.id } }, info);
 };
 
 const deleteProject = function (parent, args, context, info) {
-  console.log('arguments!!!!!', args)
   if (!args.id) throw new Error(`There is no such project`);
 
-  return context.db.mutation.deleteProject({ where: { id: args.id } }, info); //{where:{ id: args.id }}, {data: { name: args.name }}, info)
+  return context.db.mutation.deleteProject({ where: { id: args.id } }, info);
 };
+
+// Integration operations
+const addIntegration = function (parent, args, context, info) {
+  // TODO: add error handling
+  return context.db.mutation.createIntegration({
+    data: {
+      project: {
+        connect: {
+          id: args.project
+        }
+      },
+      type: args.type }
+    }, info
+  );
+};
+
+const deleteIntegration = function (parent, args, context, info) {
+  if (!args.id) throw new Error(`There is no such integration`);
+  // TODO: add error handling
+  console.log('args delete integration', args)
+  return context.db.mutation.deleteIntegration({ where: {  id: args.id } }, info );
+};
+
 
 module.exports = {
   addProject,
   updateProject,
-  deleteProject
+  deleteProject,
+  addIntegration,
+  deleteIntegration
 };
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -6,6 +6,15 @@ const projects = (parent, args, context, info) => {
   return context.db.query.projects({ where: id }, info);
 };
 
+const integrations = (parent, args, context, info) => {
+
+  // Filter on projects here
+  const id = args.id ? { id: args.id } : {};
+
+  return context.db.query.integrations({}, info);
+};
+
 module.exports = {
-  projects
+  projects,
+  integrations
 };
